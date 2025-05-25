9d8edbd4e52cb712311bf4013e18b55c2d446dd7
daivik-tapase-01 <daiviktapase@gmail.com>
2025-05-25 12:31:31 +0530

    Typecasting

---
+++ Java/Typecasting.java
@@ -0,0 +1,18 @@
+package Java;
+public class Typecasting {
+    public static void main(String[] args) {
+        // Implicit casting
+        // smaller datatype to bigger datatype
+        // programmer doen`t need specifically instruct the compiler to perform it.
+        int a = 2666;
+        double b = a;
+        System.out.println("b: " + b);
+
+        // Explicit casting
+        // bigger datatype to smaller datatype
+        // programmer needs to specifically instruct the comiler to perform it.
+        double c = 5.678;
+        int d = (int) c;
+        System.out.println("d: " + d);
+    }
+}
