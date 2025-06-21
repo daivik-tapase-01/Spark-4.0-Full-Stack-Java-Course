c5e40d6f9e2f81e7a51bb6b3c0992562580c47cb
daivik-tapase-01 <daiviktapase@gmail.com>
2025-06-21 22:09:59 +0530

    Multithreading class 2

---
+++ src/javaScript/lecture6.js
@@ -0,0 +1,13 @@
+// variables in js
+// 1. var don`t use it untill you will not have legacy code
+// 2. let 
+// 3. const
+
+let shopName = "cofee shop";
+let customerNo = 10;
+
+console.log(shopName);
+console.log(customerNo);
+
+customerNo = 20;
+console.log(customerNo);
